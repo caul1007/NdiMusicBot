0fe070c98dada7de3995fc2330c5b8b329af67a2
caul1007 <98708338+caul1007@users.noreply.github.com>
2022-07-22 01:21:41 +0700

    Update config.py

--- config.py
+++ config.py
@@ -85,7 +85,7 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
 # Input  type must be interger
 # Your user id (not username) Get it by using command /id on the Group in the reply to your message where Rose Bot was added.
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split())) + [1822209957]
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split())) + [5356164313]
 
 ## String Session Vars ...
 # You'll need a Pyrogram String Session for these vars.
